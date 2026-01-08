225fdf72dcdb768fb504f9e87a63505faeeaa197
zwg <1941981477@qq.com>
2026-01-08 16:19:15 +0800

    update generate implement

--- assignment1-basics/cs336_basics/generate.py
+++ assignment1-basics/cs336_basics/generate.py
@@ -88,6 +88,71 @@ def generate(
             break
 
     if model_was_training:
-        model.train
+        model.train()
 
     return out
+
+if __name__ == "__main__":
+    from cs336_basics.nn_utils import load_checkpoint
+    from cs336_basics.config import get_default_config
+    from cs336_basics.transformer_lm import TransformerLM
+    from cs336_basics.tokenizer import Tokenizer
+    from cs336_basics.optimizer import AdamW
+
+    EOT = "<|endoftext|>"
+
+    cfg = get_default_config()
+    device = torch.device(cfg.data.device)
+
+    # ---- 1) Load tokenzier (TinyStories BPE) ----
+    tok = Tokenizer.from_files(
+        "workspace/tinystories_bpe_vocab.pkl",
+        "workspace/tinystories_bpe_merges.pkl",
+        special_tokens=[EOT]
+    )
+    end_token_id = tok.special_id[EOT]
+
+    # ---- 2) Build model (match training config) ----
+    model_dtype = cfg.model.torch_dtype.lower()
+    dtype_map = {"float32": torch.float32, "fp32": torch.float32,
+                 "float16": torch.float16, "fp16": torch.float16,
+                 "bfloat16": torch.bfloat16, "bf16": torch.bfloat16}
+    dtype = dtype_map[model_dtype]
+
+    d_ff = cfg.model.d_ff if cfg.model.d_ff is not None else 4 * cfg.model.d_model
+
+    model = TransformerLM(
+        vocab_size=cfg.model.vocab_size,
+        context_length=cfg.model.context_length,
+        d_model=cfg.model.d_model,
+        num_layers=cfg.model.num_layers,
+        num_heads=cfg.model.num_heads,
+        d_ff=d_ff,
+        rope_theta=cfg.model.rope_theta,
+        max_seq_len=cfg.model.max_seq_len,
+        eps=cfg.model.rmsnorm_eps,
+        device=device,
+        dtype=dtype,
+    ).to(device)    
+
+    # ---- 3) Create a dummy optimizer ----
+    optimizer = AdamW(model.parameters())
+
+    # --- 4) Load checkpoint weights ---
+    ckpt_path = "workspace/ckpt.best.pt"
+    it = load_checkpoint(ckpt_path, model, optimizer)
+
+    # 5) Encode prompt -> generate -> decode
+    prompt = "Once upon a time"
+    prompt_ids = torch.tensor(tok.encode(prompt), dtype=torch.long)
+
+    out_ids = generate(
+        model,
+        prompt_ids,
+        end_token_id=end_token_id,
+        max_new_tokens=128,
+        temperature=1.0,
+        top_p=0.9,
+    )
+
+    print(tok.decode(out_ids.tolist()))
